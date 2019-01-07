6ed7afabe6d0fd4f78744d00a97a1fb5f46e14f4
worthlesscog <simon@worthlesscog.com>
2019-01-07 22:58:43 +0000

    interface + protocols;

--- src/main/scala/com/worthlesscog/tv/omdb/Interface.scala
+++ src/main/scala/com/worthlesscog/tv/omdb/Interface.scala
@@ -25,25 +25,67 @@ case class Title(
     Released: Option[String],
     Runtime: Option[String],
     Genre: Option[String],
-    Director: Option[String],
+    // Director: Option[String],
     // Writer: Option[String],
     // Actors: Option[String],
     Plot: Option[String],
     Language: Option[String],
     // Country: Option[String],
     // Awards: Option[String],
-    Poster: Option[String],
-    Ratings: Option[Seq[Rating]],
+    // Poster: Option[String],
+    // Ratings: Option[Seq[Rating]],
     // Metascore: Option[String],
     imdbRating: Option[String],
     imdbVotes: Option[String],
-    imdbID: Option[String],
+    // imdbID: Option[String],
     // Type: Option[String],
-    totalSeasons: Option[String],
-    Response: Option[String]
+    totalSeasons: Option[String] //,
+    // Response: Option[String]
 )
 
-case class Rating(
-    Source: Option[String],
-    Value: Option[String]
+// Season
+
+case class Season(
+    // Title: Option[String],
+    Season: Option[String],
+    // totalSeasons: Option[String],
+    Episodes: Seq[EpisodeSynopsis],
+    // Response: Option[String]
+)
+
+case class EpisodeSynopsis(
+    // Title: Option[String],
+    // Released: Option[String],
+    // Episode: Option[String],
+    // imdbRating: Option[String],
+    imdbID: Option[String]
+)
+
+// Episode
+
+case class Episode(
+    Title: Option[String],
+    // Year: Option[String],
+    // Rated: Option[String],
+    Released: Option[String],
+    Season: Option[String],
+    Episode: Option[String],
+    // Runtime: Option[String],
+    // Genre: Option[String],
+    Director: Option[String],
+    // Writer: Option[String],
+    Actors: Option[String],
+    Plot: Option[String],
+    // Language: Option[String],
+    // Country: Option[String],
+    // Awards: Option[String],
+    // Poster: Option[String],
+    // Ratings: Seq[Rating],
+    // Metascore: Option[String],
+    imdbRating: Option[String],
+    imdbVotes: Option[String] //,
+    // imdbID: Option[String],
+    // seriesID: Option[String],
+    // Type: Option[String],
+    // Response: Option[String]
 )
--- src/main/scala/com/worthlesscog/tv/omdb/Protocols.scala
+++ src/main/scala/com/worthlesscog/tv/omdb/Protocols.scala
@@ -4,7 +4,16 @@ import spray.json.DefaultJsonProtocol
 
 object Protocols extends DefaultJsonProtocol {
 
+    import com.worthlesscog.tv.optionStringFormat
+
     implicit val titleSynopsisFmt = jsonFormat5(TitleSynopsis)
     implicit val searchResultFmt = jsonFormat3(SearchResult)
+
+    implicit val titleFmt = jsonFormat9(Title)
+
+    implicit val episodeSynopsisFmt = jsonFormat1(EpisodeSynopsis)
+    implicit val seasonFmt = jsonFormat2(Season)
+
+    implicit val episodeFmt = jsonFormat9(Episode)
 
 }
